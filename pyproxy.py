3cdebe3a07df5ae44270fc9f9b4d05e88b32c5ce
= <john.wells@utah.edu>
2013-01-16 21:59:51 -0700

    Small change in structure.

--- pyproxy.py
+++ pyproxy.py
@@ -1,16 +1,18 @@
-import socket, sys
+import socket, sys, re, multiprocessing
 
 
+            
 def main():
     
     if len(sys.argv) <= 1:
-        print 'Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server'
+        print 'Usage : "python pyproxy.py <server_ip>"\n[server_ip : It is the IP Address Of Proxy Server]'
         sys.exit(2)
     host = ''
-    port = 80
+    port = sys.argv[1]
+        
     # Create a TCP socket connection, bind it to the specified port.
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host,port))
+    server_socket.bind((host,int(port)))
     server_socket.listen(5)
     
     #Start serving requests...
@@ -18,39 +20,71 @@ def main():
         print "Ready to serve..."
         client_socket, client_address = server_socket.accept()
         print 'Received connection request from: ', client_address
-        #TODO: Implement threading here.
-        _message = client_socket.recv(1024)
-        message = _message.split()
-        if 'GET' in message[0]:
-            method = message.split()[0]
-            url = message.split()[1]
-            http_ver = message.split()[2]
-            
-            # caching check.
-            filename = url.partition("/")[2] 
-            print filename 
-            fileExist = "false"
-            file_to_use = "/" + filename
-            print file_to_use
-            try:
-                # Check whether the file exist in the cache
-                f = open(file_to_use[1:], "r")
-                outputdata = f.readlines()
-                fileExist = "true"
-                # ProxyServer finds a cache hit and generates a response message tcpCliSock.send("HTTP/1.0 200 OK\r\n") tcpCliSock.send("Content-Type:text/html\r\n")
-                # Fill in start.
-                # Fill in end.
-            except IOError:
-                # Do something here.
-                break;
-        else:
-            print "Error: %s Not Implemented." % message[0]
-            break
-       
-def uri_parser(message):
-    pass
+        p = multiprocessing.Process(target=client_thread, args=(client_socket, client_address))
+        p.start()
+        p.join()
+        
 
+def client_thread(socket):
+    #TODO: Implement threading here.
+        _message = socket.recv(1024)
+        request = request_parser(_message)
+        if request is not None:
+            # Request is valid. Perform appropriate action.
+            build_uri(request)
+            
+            #Send request
+            
+            #Cache the results
+            pass
+        else:
+            return None
+
+def build_uri(request):
+    """
+    Takes a properly formatted HTTP request and returns the formatted URI request.
+    Refer to RFC 1945 section 5.4.2 for details on how URI requests are formatted.
+    @param request: Hash of a valid HTTP GET request.
+    @return: Valid URI
+    """
+    return """%s / %s
+Host: %s
+Connection: close
+%s
+""" % request['method'], request['version'], request['url'], request['headers']
+    pass
        
+def request_parser(message):
+    """
+    Given a message received from a connected client, determine if it is a valid HTTP request.
+    @param message: Client HTTP request. 
+    @return: Hash of valid URI fields.
+    @return: None upon encountering an error.
+    """
+    regex = re.compile(r"""(?P<method>\w+) (?<url>http.*) (?P<version>HTTP/\d\.\d) (?<headers>.*)""")
+    match = regex.match(message)
+    if match is not None:
+        _method = match.group('method')
+        _url = match.group('url')
+        _version = match.group('version')
+        _headers = match.group('headers')
+        
+        if 'GET' in _method:
+            return {'method':_method, 'url':_url, 'version':_version, 'headers': _headers}
+        else:
+            print "Error 501: Not Implemented"
+            return None
+    else:
+        print "Error 400: Bad Request"
+        return None
+        
+        
+def is_cached(filename):
+    filename = filename.partition("/")[2] 
+    print filename 
+    fileExist = "false"
+    file_to_use = "/" + filename
+    print file_to_use
 
 if __name__ == '__main__':
     main()
