09091636b1af41288b579954658eb28e30090974
= <john.wells@utah.edu>
2013-02-02 02:22:20 -0700

    Caching added. Threading implemented, but needs testing.

--- pyproxy.py
+++ pyproxy.py
@@ -1,10 +1,14 @@
-import socket, sys, re
+import socket, sys, re, threading
+from functools import partial
 from urlparse import urlparse
 
+cache = {}
+# List of all accepted URI headers. Connection is omitted because it is always replaced with 'close' anyway.
 header_fields = ['Accept', 'Accept-Charset', 'Accept-Encoding', 'Accept-Language', 'Accept-Datetime',
 'Authorization', 'Cache-Control', 'Cookie', 'Content-Length', 'Content-MD5', 'Content-Type',
 'Date', 'Expect', 'From', 'Host', 'If-Match', 'If-Modified-Since', 'If-None-Match', 'If-Range', 'If-Unmodified-Since',
-'Max-Forwards', 'Pragma', 'Proxy-Authorization', 'Range', 'Referer', 'TE', 'Upgrade', 'User-Agent', 'Via', 'Warning']      
+'Max-Forwards', 'Pragma', 'Proxy-Authorization', 'Range', 'Referer', 'TE', 'Upgrade', 'User-Agent', 'Via', 'Warning']    
+  
 def main():
    
     if len(sys.argv) <= 1:
@@ -17,52 +21,57 @@ def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, int(port)))
     server_socket.listen(5)
-    
+
     #Start serving requests...
     while True:
         print "Ready to serve..."
         client_socket, client_address = server_socket.accept()
         print 'Received connection request from: ', client_address
-        client_thread(client_socket)
+#        client_thread(client_socket)
+        t = threading.Thread(target=partial(client_thread, client_socket))
+        t.start()
 #        p = multiprocessing.Process(target=client_thread, args=(client_socket, client_address))
 #        p.start()
 #        p.join()
         
-# THIS IS MY FUNCTION
+
 def client_thread(cli_socket):
     _message = cli_socket.recv(1024)
 #        print _message
     request = request_parser(_message)
     if request is not None:
-        # Request is valid. Perform appropriate action.
-        s = build_uri(request)
-#            res = socket.getaddrinfo(request['host'], 80, 0, 0, socket.SOL_TCP)
-        outbound = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#            outbound.bind(('',0)) # accept defapydev eclult OS behavior
-#            for r in res:
-#                if r[4] is not None:
-#                    outbound.connect(r[4])
-#                    break
-        outbound.connect((request['host'], 80))
-        outbound.sendall(s)
-#        out_message = outbound.recv(4096)
-        data = ""
-        while True:
-            try:
-#                print "Receiving data..."
-                data += outbound.recv(4096)
-#                cli_socket.send(data)
-            except socket.timeout:
-                print "timed out"
-                break
-        cli_socket.sendall(data)
-        outbound.close()
-        cli_socket.close()
-#            print res
-        #Send request
+        cached_url = request['host'] + request['url']
+        if cached_url in cache:
+            print "found in cache"
+            cli_socket.sendall(cache[cached_url])
 
-        #Cache the results
-
+        else:
+            # Request is valid. Perform appropriate action.
+            s = build_uri(request)
+            outbound = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            outbound.connect((request['host'], 80))
+            outbound.send(s)
+            total_data = []
+            while True:
+                try:
+                    data = outbound.recv(8192)
+                    if not data: break
+                    total_data.append(data)
+                except socket.timeout:
+                    break
+            
+            #Send result to originating client
+            data = ''.join(total_data)
+            cli_socket.sendall(data)
+        
+            #Cache the results
+            cache[cached_url] = data
+            
+            #Close server socket
+            outbound.close()
+        
+        # Close the client socket
+        cli_socket.close() 
     else:
         return None
 
@@ -71,27 +80,28 @@ def build_uri(request):
     Takes a properly formatted HTTP request and returns the formatted URI request.
     Refer to RFC 1945 section 5.4.2 for details on how URI requests are formatted.
     @param request: Hash of a valid HTTP GET request.
-    @return: A (hopefully) Valid URI
-    """
-
-    _headers = [(_h.split(':')[0], _h.split(':')[1]) for _h in request['headers'].splitlines() if _h.split(':')[0] in header_fields]
-    _val = "".join("%s:%s\r\n" % h for h in _headers)
-    _val += "Connection: close\r\n\r\n"
-#    if '/' in url:
-#        # implement later.
-#        pass
-
-    return _val
-    
-       
+    @return: A (hopefully) valid URI
+    """  
+    _headers = [(_h.split(':')[0].strip(), _h.split(':')[1].strip()) for _h in request['headers'].splitlines() if _h.split(':')[0] in header_fields]
+    _headers.insert(0, ('Connection', 'close'))
+    if 'Host' not in _headers:
+        _headers.insert(0, ('Host', request['host']))
+        _val = "".join("%s: %s\r\n" % h for h in _headers)
+    else:
+        _val = "".join("%s: %s\r\n" % h for h in _headers)
+    _val += "\r\n"
+    # Form the URI request in its entirety. 
+    _s = "GET {0} {1} \r\n{2}".format(request['url'], request['version'], _val)
+    return _s
+         
 def request_parser(message):
     """
     Given a message received from a connected client, determine if it is a valid HTTP request.
-    @
     @param message: Client HTTP request. 
     @return: Hash of valid URI fields.
     @return: None upon encountering an error.
     """
+    # Breakdown of regex:        GET      AbsoluteURL (per rfc)        HTTP version            Everything else.
     regex = re.compile(r"""(?P<method>\w+) (?P<url>http.*) (?P<version>HTTP/\d\.\d)\r*\n*(?P<headers>.*)\r*\n""", re.DOTALL)
     match = regex.match(message)
     if match is not None:
@@ -101,7 +111,7 @@ def request_parser(message):
         _headers = match.group('headers')
         
         if 'GET' in _method:
-            return {'method':_method, 'url':_url, 'version':_version, 'headers': _headers, 'host': urlparse(_url)[1]}
+            return {'method':_method, 'url':urlparse(_url)[2], 'version':_version, 'headers': _headers, 'host': urlparse(_url)[1]}
         else:
             print "Error 501: Not Implemented"
             return None
