34534628167ee0f9e4cac75f6b328ea2f34d94e3
= <john.wells@utah.edu>
2013-02-09 19:46:29 -0700

    Finalized threading. Tested for functionality.
    Need to implement logging (as a bonus)
    Need to implement advanced caching (as a bonus)

--- pyproxy.py
+++ pyproxy.py
@@ -1,8 +1,9 @@
-import socket, sys, re, threading
+import socket, sys, re, threading, logging
 from functools import partial
 from urlparse import urlparse
 
 cache = {}
+
 # List of all accepted URI headers. Connection is omitted because it is always replaced with 'close' anyway.
 header_fields = ['Accept', 'Accept-Charset', 'Accept-Encoding', 'Accept-Language', 'Accept-Datetime',
 'Authorization', 'Cache-Control', 'Cookie', 'Content-Length', 'Content-MD5', 'Content-Type',
@@ -12,7 +13,7 @@ header_fields = ['Accept', 'Accept-Charset', 'Accept-Encoding', 'Accept-Language
 def main():
    
     if len(sys.argv) <= 1:
-        print 'Usage : "python pyproxy.py <server_ip>"\n[server_ip : It is the IP Address Of Proxy Server]'
+        print 'Usage : "python pyproxy.py <server_port>"\n[server_port : It is the desired port for the Proxy Server]'
         sys.exit(2)
     host = ''
     port = sys.argv[1]
@@ -21,41 +22,50 @@ def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, int(port)))
     server_socket.listen(5)
-
+    
+    print "Ready to serve..."
+    
     #Start serving requests...
     while True:
-        print "Ready to serve..."
+
         client_socket, client_address = server_socket.accept()
-        print 'Received connection request from: ', client_address
-#        client_thread(client_socket)
+#        print 'Received connection request from: ', client_address
+
         t = threading.Thread(target=partial(client_thread, client_socket))
         t.start()
-#        p = multiprocessing.Process(target=client_thread, args=(client_socket, client_address))
-#        p.start()
-#        p.join()
         
 
 def client_thread(cli_socket):
+    """
+    Takes a client socket and retrieves a webpage from the internet on the client's behalf.
+    Requests from client must come in the form of a valid URI.
+    @param cli_socket: Incoming client socket (host,port)
+    @see: build_uri(request) for more information on URI formatting.
+    
+    
+    """
     _message = cli_socket.recv(1024)
-#        print _message
+
     request = request_parser(_message)
-    if request is not None:
+    if 'error' not in request:
         cached_url = request['host'] + request['url']
         if cached_url in cache:
-            print "found in cache"
+            print "Found %s in cache" % cached_url
             cli_socket.sendall(cache[cached_url])
 
         else:
-            # Request is valid. Perform appropriate action.
+            # Request is valid and not cached. Perform appropriate action.
             s = build_uri(request)
             outbound = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print request['host']
             outbound.connect((request['host'], 80))
             outbound.send(s)
             total_data = []
             while True:
                 try:
                     data = outbound.recv(8192)
-                    if not data: break
+                    if not data:
+                        break
                     total_data.append(data)
                 except socket.timeout:
                     break
@@ -73,7 +83,9 @@ def client_thread(cli_socket):
         # Close the client socket
         cli_socket.close() 
     else:
+        cli_socket.send(request['error'])
         return None
+
 
 def build_uri(request):
     """
@@ -84,12 +96,15 @@ def build_uri(request):
     """  
     _headers = [(_h.split(':')[0].strip(), _h.split(':')[1].strip()) for _h in request['headers'].splitlines() if _h.split(':')[0] in header_fields]
     _headers.insert(0, ('Connection', 'close'))
+    
+    # If a Host: header was not present in the client's request, build one manually.
     if 'Host' not in _headers:
         _headers.insert(0, ('Host', request['host']))
-        _val = "".join("%s: %s\r\n" % h for h in _headers)
-    else:
-        _val = "".join("%s: %s\r\n" % h for h in _headers)
+#        _val = "".join("%s: %s\r\n" % h for h in _headers)
+#    else:
+    _val = "".join("%s: %s\r\n" % h for h in _headers)
     _val += "\r\n"
+    
     # Form the URI request in its entirety. 
     _s = "GET {0} {1} \r\n{2}".format(request['url'], request['version'], _val)
     return _s
@@ -113,11 +128,10 @@ def request_parser(message):
         if 'GET' in _method:
             return {'method':_method, 'url':urlparse(_url)[2], 'version':_version, 'headers': _headers, 'host': urlparse(_url)[1]}
         else:
-            print "Error 501: Not Implemented"
-            return None
+            return {'error': "Error 501: Not Implemented"}
     else:
-        print "Error 400: Bad Request"
-        return None
+        return {'error': "Error 400: Bad Request"}
+      
 
             
 def is_cached(filename):
