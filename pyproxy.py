497cf183023e86ba0fa45ebf229aa6f51265f128
= <john.wells@utah.edu>
2013-03-12 23:25:32 -0600

    Fixed relative pathing for root-level sites

--- pyproxy.py
+++ pyproxy.py
@@ -6,26 +6,26 @@ from urlparse import urlparse
 header_fields = ['Accept', 'Accept-Charset', 'Accept-Encoding', 'Accept-Language', 'Accept-Datetime',
 'Authorization', 'Cache-Control', 'Cookie', 'Content-Length', 'Content-MD5', 'Content-Type',
 'Date', 'Expect', 'From', 'Host', 'If-Match', 'If-Modified-Since', 'If-None-Match', 'If-Range', 'If-Unmodified-Since',
-'Max-Forwards', 'Pragma', 'Proxy-Authorization', 'Range', 'Referer', 'TE', 'Upgrade', 'User-Agent', 'Via', 'Warning']    
+'Max-Forwards', 'Pragma', 'Proxy-Authorization', 'Range', 'Referrer', 'TE', 'Upgrade', 'User-Agent', 'Via', 'Warning']    
 
 
 class TCPRequestHandler(SocketServer.BaseRequestHandler):
     
-    cached_sites = {}
-    
+
     def handle(self):
         """
-        Takes a client socket and retrieves a webpage from the internet on the client's behalf.
+        Entry point for multi-processing. Uses the inherited class functions to obtain the client socket.
         Requests from client must come in the form of a valid URI.
-        @param cli_socket: Incoming client socket (host,port)
         @see: build_uri(cli_request) for more information on URI formatting.
         """
         _message = self.request.recv(1024)
         cli_request = self.request_parser(_message)
         if 'error' not in cli_request:
-    
+            
+            # Attempt to load from cache. If the file doesn't exist or is corrupt, throw an IOError exception
+            # and reload from the remote server.
             try:
-  
+                
                 f = open("cache/" + cli_request['host'] + cli_request['url'].replace("/", "-"), 'rb')         
                 data = f.readlines()
                 f.close()
@@ -47,14 +47,18 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
                     logging.info("Client %s requested host %s" % (self.client_address, cli_request['host']))
                     outbound.connect((cli_request['host'], 80))
     
+                    # Write the socket buffer to a temporary file, and then read those lines into a buffer.
                     try:
                         fileobj = outbound.makefile('r', 0)
+                        print s
                         fileobj.write(s)
                        
                         buffer = fileobj.readlines()
-                                                
+                        print buffer
+                        # Cache the result.                        
                         tmpFile = open("cache/" + cli_request['host'] + cli_request['url'].replace("/", "-"), 'wb')
-    
+                        
+                        # Send the data to the requesting client.
                         for i in range(0, len(buffer)):
                             tmpFile.write(buffer[i])
                             self.request.send(buffer[i])
@@ -82,14 +86,9 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
         @return: A (hopefully) valid URI
         """  
         _headers = [(_h.split(':')[0].strip(), _h.split(':')[1].strip()) for _h in request['headers'].splitlines() if _h.split(':')[0] in header_fields]
-        _headers.insert(0, ('Connection', 'close'))
+        _headers.append(('Connection', 'close'))
         
-        # If a Host: header was not present in the client's request, build one manually.
-        if 'Host' not in _headers:
-            _headers.insert(0, ('Host', request['host']))
-            _val = "".join("%s: %s\r\n" % h for h in _headers)
-        else:
-            _val = "".join("%s: %s\r\n" % h for h in _headers)
+        _val = "".join("%s: %s\r\n" % h for h in _headers)
             
         _val += "\r\n"
         
@@ -97,7 +96,7 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
         if len(request['url']) > 1:
             _s = "GET http://{0} {1}\r\n{2}".format(request['host'] + request['url'], request['version'], _val)
         else:
-            _s = "GET http://{0} {1}\r\n{2}".format(request['host'], request['version'], _val)
+            _s = "GET / {1}\r\n{2}".format(request['host'], request['version'], _val)
         return _s
              
     def request_parser(self, message):
@@ -132,16 +131,23 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
         return """<html><head></head><body><font size="20"><b>{0}</b></font></body></html>""".format(error_message)
     
     def cache_control(self, data):
-        
+        """
+        Determines if the current file is still within the appropriate age to be considered valid.
+        @param data: data buffer from a cached file 
+        @return: True if cache file is still valid, false otherwise.
+        """
+        # List of good words and bad words. Bad words imply that the file must always be refreshed.
         _stopwords = ['no-cache', 'no-store', 'must-revalidate', 'proxy-revalidate']
         _gowords = ['max-age', 's-maxage']
         cache_control = [(d) for d in data if 'Cache-Control' in d]
         cache_control = '\n'.join(cache_control)
-#        print "cache_control: ", cache_control
+
         for s in _stopwords or not cache_control:
             if s in cache_control:
                 return False
         
+        # Attempt to retrieve max-age from the cache-control field. 
+        # Throws an exception if there is strange formatting (eg, not a number).
         max_age = None
         for g in _gowords:
             if g in cache_control:
@@ -163,6 +169,9 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
             return True
      
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    """
+    Overloaded constructor for a threadedTCPServer object. Template acquired via python.org's docs.
+    """
     pass
 
 if __name__ == '__main__':
